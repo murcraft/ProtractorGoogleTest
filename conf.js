3bd9887eaa1cf977dd52ed6b3bac9cb64955dbfb
murcraft <ogulikpurse@gmail.com>
2018-09-26 13:16:30 +0300

    Was added reporter dependency

--- conf.js
+++ conf.js
@@ -1,3 +1,8 @@
+let Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
+let today = new Date()
+let timeStamp = today.getMonth() + 1 + '-' + today.getDate() + '-' + today.getFullYear() + '-' +
+  today.getHours() + 'h-' + today.getMinutes() + 'm-' + today.getSeconds() + 's'
+
 exports.config = {
   directConnect: true,
 
@@ -10,24 +15,36 @@ exports.config = {
   multiCapabilities: [{
     browserName: 'firefox',
   },
-  {
-    browserName: 'chrome',
-    shardTestFiles: true,
-    chromeOptions: {
-      args: [
-        '--disable-infobars',
-        '--disable-extensions',
-        'verbose'
-      ]},
+    {
+      browserName: 'chrome',
+      shardTestFiles: true,
+      chromeOptions: {
+        args: [
+          '--disable-infobars',
+          '--disable-extensions',
+          'verbose'
+        ]
+      },
       prefs: {
         'profile.password_manager_enabled': false,
         'credentials_enable_service': false,
         'password_manager_enabled': false
       }
-  }],
+    }],
 
   onPrepare: () => {
+    browser.manage().window().setSize(1024, 800)
     browser.waitForAngularEnabled(false)
+    jasmine.getEnv().addReporter(
+      new Jasmine2HtmlReporter({
+        savePath: 'reports/' + timeStamp,
+        consolidate: true,
+        consolidateAll: true,
+        takeScreenshot: true,
+        screenshotsFolder: 'screenshots',
+        takeScreenshotsOnlyOnFailures: true
+      })
+    );
   },
 
   jasmineNodeOpts: {
