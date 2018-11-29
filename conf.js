06fe9e4878a795b9dfd3512e2305d420a4dc186f
Helen <ogulikpurse@gmail.com>
2018-11-29 17:01:55 +0300

    Added pretty page object

--- conf.js
+++ conf.js
@@ -1,4 +1,4 @@
-let Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
+const AllureReporter = require('jasmine-allure-reporter')
 
 exports.config = {
 
@@ -14,9 +14,15 @@ exports.config = {
     waitTimeout: 60000
   },
 
+  specs: [
+    'lib/spec/**/*.js'
+  ],
+
   suites: {
-    googlePage: 'spec/pageObjectSpec.js'
+    googlePage: 'lib/spec/pageObjectSpec.js'
   },
+
+  baseUrl: process.env.env = 'http://www.google.by',
 
   multiCapabilities: [{
       ignoreProtectedModeSettings: true,
@@ -37,33 +43,76 @@ exports.config = {
         download: {
           prompt_for_download: false,
           directory_upgrade: true,
-          default_directory: pathDownloads
         }
       }
     }
   ],
 
-  onPrepare: () => {
+  beforeLaunch: function () {
+    // global.Logger = require('./lib/helpers/logger')
+  },
 
-    return new Promise(function (fulfill, reject) {
-      browser.getCapabilities().then(function (value) {
-        reportName = value.get('browserName') + '_' + Math.floor(Math.random() * 1E16)
-        jasmine.getEnv().addReporter(
-          new Jasmine2HtmlReporter({
-            savePath: __dirname + '/reports',
-            docTitle: 'Web UI Test Report',
-            screenshotsFolder: '/image',
-            takeScreenshotsOnlyOnFailures: true,
-            consolidate: true,
-            consolidateAll: true,
-            preserveDirectory: true,
-            fileName: 'my-report.html',
-            fileNamePrefix: reportName
-          })
-        )
-        fulfill()
-      })
+  onPrepare: async () => {
+    browser.waitForAngularEnabled(false)
+    global.EC = protractor.ExpectedConditions
+    global.Logger = require('./lib/helpers/logger')
+
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultDir: 'allure-results'
+    }))
+
+    jasmine.getEnv().addReporter(new function () {
+      this.jasmineStarted = function (summary) {
+        global.TOTAL = summary.totalSpecsDefined
+        global.PASSED = 0
+        global.FAILED = 0
+        global.SKIPPED = 0
+        Logger.info(`!----------Tests started. Total tests: ${TOTAL}----------!`)
+      }
+      this.suiteStarted = function (result) {
+        Logger.info(`--------------------------------------------------`)
+        Logger.info(`Suite starts: ${result.fullName}`)
+        Logger.info(`--------------------------------------------------`)
+      }
+      this.specStarted = function (result) {
+        Logger.info(`Spec starts: ${result.description}`)
+      }
+      this.specDone = function (result) {
+        if (result.status === 'failed') {
+          FAILED++
+          Logger.failed(result)
+        }
+        if (result.status === 'passed') {
+          PASSED++
+        }
+        if (result.status === 'disabled' || result.status === 'pending') {
+          SKIPPED++
+        }
+      }
+      this.jasmineDone = function () {
+        Logger.info(`**************************************************`)
+        Logger.info(`${PASSED + FAILED + SKIPPED} of ${TOTAL} tests done`)
+        Logger.info(`Passed: ${PASSED}`)
+        Logger.info(`Failed: ${FAILED}`)
+        Logger.info(`Skipped: ${SKIPPED}`)
+        Logger.info(`**************************************************`)
+      }
     })
+
+    jasmine.getEnv().afterEach(async function () {
+      await Logger.LogConsoleErrors()
+      try {
+        await browser.takeScreenshot().then(function (png) {
+          allure.createAttachment('Screenshot', function () {
+            return new Buffer(png, 'base64')
+          }, 'image/png')()
+        })
+      } catch (e) {
+        Logger.error(`Screen shot was not taken\n${e}`)
+      }
+    })
+
+    await browser.get('')
 
   }
 }
