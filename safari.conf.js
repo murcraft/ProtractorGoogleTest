b7d30434c65e431c7e83dbce6db2c63c95011d86
Murcraft <ogulikpurse@gmail.com>
2019-04-12 12:47:55 +0300

    Deleted selenium address from safari.conf

--- safari.conf.js
+++ safari.conf.js
@@ -9,7 +9,7 @@ let downloads = keyVars.downloadPath
 
 exports.config = {
 
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+  // seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
 
   allScriptsTimeout: 110000000,
   SELENIUM_PROMISE_MANAGER: false,
