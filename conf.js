2a047a18bcfe5cac60bfc206c5d4a5973a389932
perchwell e2e <perchwell.e2e@gmail.com>
2019-05-14 22:29:05 +0300

    Edited travis

--- conf.js
+++ conf.js
@@ -200,7 +200,6 @@ let config = {
 }
 
 config.capabilities = capabilitiesMap[browserName]
-console.log(config.capabilities)
 
 if (browserName === 'firefox') {
   config.seleniumAddress = 'http://127.0.0.1:4444/wd/hub'
