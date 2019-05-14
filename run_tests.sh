35b262dc0c8a18449da1bca234e5371442cd6614
Murcraft <ogulikpurse@gmail.com>
2019-05-14 11:22:29 +0300

    Edited tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@ set -e -u
 echo "Running tests on $browser"
 if [[ $browser == "firefox" ]]; then npm run test-gecko; fi
 if [[ $browser == "chrome" ]]; then npm run test-chrome; fi
-#if [[ $browser == "safari" ]]; then osascript /Users/travis/build/murcraft/ProtractorGoogleTest/setSafariPath.scpt; fi
 if [[ $browser == "safari" ]]; then npm run test-safari; fi
 npm test || if [$? != 0]; then exit 0; fi
