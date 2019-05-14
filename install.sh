5078704652a115a2edcf30f3434b76bca17166df
Murcraft <ogulikpurse@gmail.com>
2019-05-14 12:17:22 +0300

    Added script before

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 set -e -u
+echo "Install dependencies for $TRAVIS_OS_NAME"
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
-    # Install some custom requirements on macOS
-     echo "Install MAC OS display values"
-     brew /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x16
-else
-     echo "Install Linux display values"
-    /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1920x1080x16
+chmod +x ./install-safari.sh
+chmod +x ./run_tests.sh
+if [ $TRAVIS_OS_NAME == "osx" ]; then
+  sudo Xvfb :99 -ac -screen 0 1920x1080x16
+  echo "ok"
+  bash ./install-safari.sh
 fi
+if [ $TRAVIS_OS_NAME == "linux" ]; then
+  sh -e /etc/init.d/xvfb start
+  then sleep 3
+fi
+if [ $TRAVIS_BUILD_STAGE_NAME == "Chrome" ]; then export browser=chrome; fi
+if [ $TRAVIS_BUILD_STAGE_NAME == "Firefox" ]; then export browser=firefox; fi
+if [ $TRAVIS_BUILD_STAGE_NAME == "Safari" ]; then export browser=safari; fi
+ls ~/Library/Safari
