a0d3bdb2b755e9f94065c35c8f074788f267525f
Murcraft <ogulikpurse@gmail.com>
2019-05-15 10:07:51 +0300

    Added safari prefs in travis file

--- install-safari.sh
+++ install-safari.sh
@@ -11,6 +11,15 @@ killall "$SAFARI_NAME"
 
 # Tell Safari not to restore the browser windows when it is relaunched
 defaults write com.apple.$SAFARI_SHORT_NAME ApplePersistenceIgnoreState YES
+defaults write com.apple.Safari IncludeDevelopMenu YES
+#defaults write com.apple.Safari DisableLocalFileRestrictions YES
+#defaults write com.apple.Safari WebKitAllowFileAccessFromFileURLs YES
+#defaults write com.apple.Safari WebKitAllowUniversalAccessFromFileURLs YES
+#defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey YES
+#defaults write com.apple.Safari.ContentPageGroupIdentifier.WebKit2AllowFileAccessFromFileURLs YES
+#defaults write com.apple.Safari.ContentPageGroupIdentifier.WebKit2AllowUniversalAccessFromFileURLs YES
+#defaults write com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled YES
+#defaults write com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles YES
 
 # Turn on fake devices
 defaults write com.apple.$SAFARI_SHORT_NAME WebKitMockCaptureDevicesEnabled 1
@@ -25,4 +34,5 @@ defaults write com.apple.$SAFARI_SHORT_NAME WebKitJavaScriptCanOpenWindowsAutoma
 defaults write com.apple.$SAFARI_SHORT_NAME com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool true
 
 # Turn on Allow Remote Automation. This only works in Mac OS 10.12.6+
+safaridriver --help
 sudo safaridriver --enable --diagnose
