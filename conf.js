601778b0f6c92db12c22a5c3b67bdfd492333f6c
Murcraft <ogulikpurse@gmail.com>
2019-05-17 14:33:20 +0300

    Added ls dir in core

--- conf.js
+++ conf.js
@@ -208,7 +208,7 @@ let config = {
 
     if (browserName === 'safari') {
       try {
-        console.log(`Get all ${browserName} crashes:\n ${child_process.execSync(`ls ~/Library/Logs/`)}`)
+        console.log(`Get all ${browserName} crashes:\n ${child_process.execSync(`ls ~/Library/Logs/CoreSimulator`)}`)
         console.log(`Get all crashes:\n ${child_process.execSync(`cp -av ~/Library/Logs/DiagnosticReports/* ~/build/murcraft/ProtractorGoogleTest/artifacts/`)}`)
       } catch (e) {
         console.log(`Error executing the command\n${e}`)
